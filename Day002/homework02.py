603d7458b631076ee8bf59f5730502816f43f647
jasonbu163 <jasonbu163@163.com>
2022-06-20 17:56:19 +0800

    修改了代码表示以及练习项目的文件名

--- Day002/homework02.py
+++ Day002/homework02.py
@@ -2,12 +2,19 @@
 Author: jasonbu163 jasonbu163@163.com
 Date: 2022-06-16 22:49:38
 LastEditors: jasonbu163 jasonbu163@163.com
-LastEditTime: 2022-06-17 16:30:17
-FilePath: \Python_learning_Core_code\Day002\exercise02.py
+LastEditTime: 2022-06-20 17:39:01
+FilePath: \Python_learning_Core_code\Day002\homework02.py
 Description: 输入元的半径计算周长和面积
 
 Copyright (c) 2022 by jasonbu163 jasonbu163@163.com, All Rights Reserved. 
 '''
+import math
+
+radius = float(input('请输入圆的半径：'))
+perimeter = 2 * math.pi * radius
+area = math.pi * radius ** 2
+print(f'圆的周长：{perimeter:.4f}')
+print(f'圆的面积：{area:.4f}')
 
 # 我的答案
 
@@ -25,3 +32,4 @@ perimeter = 2 * 3.1416 * radius
 area = 3.1416 * radius * radius
 print('周长：%.2f' % perimeter)
 print('面积：%.2f' % area)
+
--- Day003/homework01.py
+++ Day003/homework01.py
@@ -2,8 +2,8 @@
 Author: jasonbu163 jasonbu163@163.com
 Date: 2022-06-17 16:34:14
 LastEditors: jasonbu163 jasonbu163@163.com
-LastEditTime: 2022-06-17 16:34:31
-FilePath: \Python_learning_Core_code\Day003\exercise01.py
+LastEditTime: 2022-06-20 17:54:15
+FilePath: \Python_learning_Core_code\Day003\homework01.py
 Description: 英制单位英寸和公制单位厘米互换
 
 Copyright (c) 2022 by jasonbu163 jasonbu163@163.com, All Rights Reserved. 
@@ -12,9 +12,11 @@ Copyright (c) 2022 by jasonbu163 jasonbu163@163.com, All Rights Reserved.
 value = float(input('请输入长度：'))
 unit = input('请输入单位：')
 
-if unit == 'in' or unit == '英尺':
-    print('%f英尺 = %f厘米' % (value, value * 2.54))
+if unit == 'in' or unit == '英寸':
+    print('%.2f英寸 = %.2f厘米' % (value, value * 2.54))
+    print(f'{value:.2f}英寸 = {value * 2.54:.2f}厘米')
 elif unit == 'cm' or unit == '厘米':
-    print('%f厘米 = %f英尺' % (value, value / 2.54))
+    print('%.2f厘米 = %.2f英寸' % (value, value / 2.54))
+    print(f'{value:.2f}厘米 = {value / 2.54:.2f}英寸')
 else:
     print('请输入有效的单位')
