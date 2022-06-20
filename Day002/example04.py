19dc48e15c3f1db0f96789d8bb149bbe7c0177e2
jasonbu163 <jasonbu163@163.com>
2022-06-20 17:26:13 +0800

    新增一些例子和解析

--- Day002/example04.py
+++ Day002/example04.py
@@ -2,18 +2,46 @@
 Author: jasonbu163 jasonbu163@163.com
 Date: 2022-06-16 22:49:38
 LastEditors: jasonbu163 jasonbu163@163.com
-LastEditTime: 2022-06-17 16:40:48
+LastEditTime: 2022-06-20 17:00:09
 FilePath: \Python_learning_Core_code\Day002\example04.py
 Description: 赋值运算符和符合赋值运算符
+~ 赋值运算符：右边赋值给左边（变量） --->  = 
+~ 算术运算符 --->  + - * / // % **
+~ 符合的算术运算符 --->  += -= *= /= //= %= **=
+~ 关系运算符（比较运算符） --->  > < >= <= == != 
+~ 逻辑运算符：把多个布尔值处理成一个布尔值（做多个布尔值的组合） --->  and（与） / or（或） / not（非）
 
 Copyright (c) 2022 by jasonbu163 jasonbu163@163.com, All Rights Reserved. 
 '''
 
 a = 10
 b = 3
-a += b      #相当于：a = a + b
-a *= a + 2  #相当于：a = a * (a + 2)
-
+a += b      # 相当于：a = a + b
+a *= a + 2  # 相当于：a = a * (a + 2)
+# Python中没有a++这种写法
 print(a)
 
-# Python中没有a++这种写法
+print('--------------------------------')
+
+print(a > b)
+print(a != b)
+print(a <= b)
+
+print('--------------------------------')
+
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+print('--------------------------------')
+
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+
+print('--------------------------------')
+
+print(not True)
+print(not False)
--- Day002/example05.py
+++ Day002/example05.py
@@ -2,9 +2,16 @@
 Author: jasonbu163 jasonbu163@163.com
 Date: 2022-06-16 22:49:38
 LastEditors: jasonbu163 jasonbu163@163.com
-LastEditTime: 2022-06-17 16:29:49
+LastEditTime: 2022-06-20 17:07:54
 FilePath: \Python_learning_Core_code\Day002\example05.py
 Description: 比较运算符和逻辑运算符的使用
+~ and和or两个运算符有短路功能，因此也被称为短路运算符
+
+短路运算符就是从左到右的运算中前者满足要求，就不再执行后者了； 可以理解为：
+
+    &&为取假运算，从左到右依次判断，如果遇到一个假值，就返回假值，以后不再执行，否则返回最后一个真值；
+
+    || 为取真运算，从左到右依次判断，如果遇到一个真值，就返回真值，以后不再执行，否则返回最后一个假值。
 
 Copyright (c) 2022 by jasonbu163 jasonbu163@163.com, All Rights Reserved. 
 '''
@@ -21,3 +28,13 @@ print('flag2 =', flag2)
 print('flag3 =', flag3)
 print('flag4 =', flag4)
 print('flag5 =', flag5)
+
+print('-----------------------------')
+
+a = int(input('a = '))
+flag1 = a > 50
+flag2 = a % 2 == 0
+
+print(flag1, flag2)
+print(flag1 and flag2)
+print(a > 50 and a % 2 == 0)
