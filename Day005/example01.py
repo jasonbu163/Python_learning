c5a671a675908f119a1bfa83ca11bcf4061b8a34
jasonbu163 <jasonbu163@163.com>
2022-06-30 11:30:58 +0800

    更新了数据分析的学习笔记

---
+++ Day005/example01.py
@@ -0,0 +1,14 @@
+'''
+Author: jasonbu163 jasonbu163@163.com
+Date: 2022-06-24 21:18:05
+LastEditors: jasonbu163 jasonbu163@163.com
+LastEditTime: 2022-06-25 19:55:11
+FilePath: \Python_learning_Core_code\Day005\example01.py
+Description: 将一颗骰子掷60000次，统计出每一面出现的次数
+
+import random
+
+random.randrange(1, 7) ---> 用1-6的随机数模拟掷骰子
+
+Copyright (c) 2022 by jasonbu163 jasonbu163@163.com, All Rights Reserved. 
+'''
